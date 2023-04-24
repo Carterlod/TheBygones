99d020118b41aef71cbee000ab4446c0edbdaaed
Carter <callod@gmail.com>
2023-04-24 01:08:26 -0700

    better blinking

--- Assets/Blinking.cs
+++ Assets/Blinking.cs
@@ -5,22 +5,24 @@ using UnityEngine;
 public class Blinking : MonoBehaviour
 {
     private Coroutine countdownRoutine;
-    SkinnedMeshRenderer eyeRenderer;
+    [SerializeField] SkinnedMeshRenderer eyeRenderer;
 
     [SerializeField] float blinkDuration = 1f;
     [SerializeField] float delayTilNextBlink = 2;
     [SerializeField] int blinkBlendShapeIndex = 0;
     [SerializeField] AnimationCurve curve;
+    
     float t = 0;
 
     private void Start()
 
     {
-        eyeRenderer = GetComponent<SkinnedMeshRenderer>();
-        //StartCoroutine(CountdownRoutine());
+        //eyeRenderer = GetComponent<SkinnedMeshRenderer>();
+        StartCoroutine(BlinkRoutine());
 
     }
 
+    /*
     private void Update()
     {
         t += Time.deltaTime;
@@ -32,6 +34,26 @@ public class Blinking : MonoBehaviour
         eyeRenderer.SetBlendShapeWeight(blinkBlendShapeIndex, curve.Evaluate(t));
         Debug.Log("t = " + t);
     }
+    */
 
+    IEnumerator BlinkRoutine()
+    {
+        Debug.Log("Starting new routine");
+        while(t < blinkDuration)
+        {
+            t += Time.deltaTime;
+            Debug.Log("t = " + t);
+            eyeRenderer.SetBlendShapeWeight(blinkBlendShapeIndex, curve.Evaluate(t/blinkDuration));
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(delayTilNextBlink);
+
+
+        t = 0;
+        StartCoroutine(BlinkRoutine());
+
+        yield return null;
+    }
     
 }
