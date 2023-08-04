577df28517c285eaad8019b2a8ca6167f6e28b90
Carter <callod@gmail.com>
2023-08-04 12:07:10 -0700

    sky added to environment props, EPs update in editor now

--- Assets/Scripts/EnvironmentProperties.cs
+++ Assets/Scripts/EnvironmentProperties.cs
@@ -2,19 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class EnvironmentProperties : MonoBehaviour
 {
     [SerializeField] Color ambientColor;
     [SerializeField] Color fogColor = Color.white;
+    [SerializeField] Color skyColor = Color.white;
     [SerializeField] GameObject probeParent;
     [SerializeField] float reflectionProbeIntensity;
+    private void OnValidate()
+    {
+        UpdateLighting();
+    }
     private void OnEnable()
+    {
+        UpdateLighting();
+    }
+
+    private void UpdateLighting()
     {
         RenderSettings.ambientLight = ambientColor;
         RenderSettings.fogColor = fogColor;
+        RenderSettings.skybox.SetColor("_Tint", skyColor);
 
         ReflectionProbe[] probes = probeParent.GetComponentsInChildren<ReflectionProbe>();
-        foreach(ReflectionProbe probe in probes)
+        foreach (ReflectionProbe probe in probes)
         {
             probe.intensity = reflectionProbeIntensity;
         }
