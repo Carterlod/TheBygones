b7c6cb59de5d894dd941e688a4b3573f682248c1
Carter <callod@gmail.com>
2023-09-20 11:48:16 -0700

    bread eating

---
+++ Assets/Scripts/Toast.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toast : MonoBehaviour
+{
+    [SerializeField] Grabbable grabbable;
+    [SerializeField] GameObject[] toastMeshes;
+    [SerializeField] int toastStep = 0;
+    private bool toastEaten = false;
+    private void Update()
+    {
+        if (grabbable.grabbed && !toastEaten)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                TakeBite();
+            }
+        }
+    }
+    private void TakeBite()
+    {
+        toastStep += 1;
+        if(toastStep> toastMeshes.Length - 1)
+        {
+            ToastDone();
+        }
+        else
+        {
+            foreach(GameObject toastVersion in toastMeshes)
+            {
+                toastVersion.SetActive(false);
+            }
+            toastMeshes[toastStep].SetActive(true);
+        }
+    }
+    private void ToastDone()
+    {
+        toastEaten = true;
+        PlayerSettings.i.objectGrabber.Release(transform);
+        this.gameObject.SetActive(false);
+    }
+}
