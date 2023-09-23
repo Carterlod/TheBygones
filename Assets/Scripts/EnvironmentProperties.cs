b9e11048d6baf8208ba0bcf471622aa51ab693ea
iane <lynlisten@gmail.com>
2023-09-23 14:17:43 -0700

    environment property toonramptint

--- Assets/Scripts/EnvironmentProperties.cs
+++ Assets/Scripts/EnvironmentProperties.cs
@@ -8,8 +8,10 @@ public class EnvironmentProperties : MonoBehaviour
     [SerializeField] Color ambientColor;
     [SerializeField] Color fogColor = Color.white;
     [SerializeField] Color skyColor = Color.white;
+    [SerializeField] Color toonRampTint = new Color(0.2f, 0.2f, 0.2f);
     [SerializeField] GameObject probeParent;
     [SerializeField] float reflectionProbeIntensity;
+    
     private void OnValidate()
     {
         UpdateLighting();
@@ -25,6 +27,8 @@ public class EnvironmentProperties : MonoBehaviour
         RenderSettings.fogColor = fogColor;
         RenderSettings.skybox.SetColor("_Tint", skyColor);
 
+        Shader.SetGlobalColor("GLOBAL_ToonRampTint", toonRampTint);
+        
         ReflectionProbe[] probes = probeParent.GetComponentsInChildren<ReflectionProbe>();
         foreach (ReflectionProbe probe in probes)
         {
